25eaa596890db32dc633a4553c86b00806aa681d
MahmoudRizk <mahmoud.rizk@lavaloon.com>
2022-05-14 19:11:08 +0200

    Add demo data.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,67 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Administrator
+        DB::table('users') -> insert([ 
+            'id' => 1, 
+            'name' => 'Administrator',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('12345678')
+        ]);
+        
+        // Customer
+        DB::table('users') -> insert([  
+            'id' => 2,
+            'name' => 'MRizk',
+            'email' => 'mrizk@test.com',
+            'password' => Hash::make('12345678')
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Apply user roles
+        // TODO: find elegant way for assigning user_id instead of hard coding it. Maybe use data factories.
+        DB::table('user_assigned_roles') -> insert([  
+            'user_id' => 1,
+            'role_name' => 'Admin'
+        ]);
+
+        DB::table('user_assigned_roles') -> insert([  
+            'user_id' => 2,
+            'role_name' => 'Customer'
+        ]);
+        
+        // Add cities
+        DB::table('cities') -> insert([
+            'id' => 1,  
+            'city_name' => 'Cairo',
+            'city_order' => '10'
+        ]);
+        
+        DB::table('cities') -> insert([
+            'id' => 2,  
+            'city_name' => 'AlFayyum',
+            'city_order' => '20'
+        ]);
+        
+        DB::table('cities') -> insert([ 
+            'id' => 3, 
+            'city_name' => 'AlMinya',
+            'city_order' => '30'
+        ]);
+        
+        DB::table('cities') -> insert([
+            'id' => 4,  
+            'city_name' => 'Asyut',
+            'city_order' => '40'
+        ]); 
+
+
+        // Add first trip
+        DB::table('trips') -> insert([  
+            'trip_name' => 'First Trip',
+            'from_city_id' => 1,
+            'to_city_id' => 4,
+            'max_seats_number' => 12
+        ]);
+
     }
 }
